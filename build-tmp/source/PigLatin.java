69da4d5a9006db2dafb34d12cd08a2e039cb2739
unknown <thwhite@697-334-Linhir.sf.k12.internal>
2015-01-07 10:45:44 -0800

    First Commit

--- build-tmp/source/PigLatin.java
+++ build-tmp/source/PigLatin.java
@@ -1,4 +1,22 @@
-import java.util.*;
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class PigLatin extends PApplet {
+
+
 
 public void setup() {
 	String lines[] = loadStrings("LowellHymn.txt");
@@ -57,8 +75,18 @@ public String pigLatinLine(String sWord) {
 			translatedString = translatedString + " " + pigLatinWord(sWord.substring(previousWord, i));
 			previousWord = i + 1;
 		}
+
 	}
 	if (translatedString.equals("")) {return "LINE_ERROR";}
 	return translatedString;
 }
 //Another function that uses pigLatinWord for each word.
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "PigLatin" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
